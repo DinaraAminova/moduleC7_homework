d4273ff63f526685514cd62ac15cfb001012075f
DinaraAminova <98523810+DinaraAminova@users.noreply.github.com>
2022-06-05 20:15:32 +0500

    Update revertString.test.js

--- С7.6/src/revertString.test.js
+++ С7.6/src/revertString.test.js
@@ -1,13 +1,5 @@
-import { revertString } from './revertStr';
+import { revertString } from '../revertStr';
 
-describe('test for revertString function', () => {
-    const firstStr = "Hello!";
-    const revFirstStr = "!olleH";
-    const secondStr = "Hello, world!";
-    const revSecondStr = "!dlrow ,olleH";
-
-    it ('should return reversed "Hello!" and "Hello, world!"', () => {
-        expect(revertString(firstStr)).toBe(`The result is ${revFirstStr}`);
-        expect(revertString(secondStr)).toBe(`The result is ${revSecondStr}`)
-    });
-})
+describe("tests for revertString function", () => {
+  it("should reverse string", () => expect(revertString("abc")).toBe("cba"));
+});
